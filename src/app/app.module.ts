a3d447d73bc7f831a28348462cd74da295c4fd45
baumannalexj <baumannalexj@gmail.com>
2018-10-07 16:59:04 -0500

    added  imports to app module, and added albumcompoenent and artistcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router'
+import {LocationStrategy, HashLocationStrategy, APP_BASE_HREF} from '@angular/common'
 
 import { AppComponent } from './app.component';
+import { AlbumComponentComponent } from './album-component/album-component.component';
+import { ArtistComponentComponent } from './artist-component/artist-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponentComponent,
+    ArtistComponentComponent
   ],
   imports: [
     BrowserModule
