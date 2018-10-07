05c6aa51867ff541e0d6e726c67aedec408d0c98
baumannalexj <baumannalexj@gmail.com>
2018-10-07 16:59:13 -0500

    added  imports to app module, and added albumcompoenent and artistcomponent

---
+++ src/app/artist-component/artist-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-component',
+  templateUrl: './artist-component.component.html',
+  styleUrls: ['./artist-component.component.css']
+})
+export class ArtistComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
